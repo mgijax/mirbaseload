5b867a437c337b807350bc851064b718b11c47c1
Lori Corbani <Lori.Corbani@jax.org>
2006-05-03 17:40:54 +0000

    TR 7651

---
+++ mirbaseload.sh
@@ -0,0 +1,138 @@
+#!/bin/sh -f -x
+
+#
+# miRBase Load Jobstream Wrapper
+#
+# Usage:
+# 	mirbaseload.sh
+#
+# Purpose:
+#
+# 	Parse the MGI-generated file MicroRNAload into these output files:
+#		1.  association marker/mirbase loader file
+#		2.  coordinate loader file
+#		3.  mapping loader file
+#
+#	We only load one coordinate per Marker.
+#
+#       (mirbaseparse.py)
+#
+#	Load the output files into the database.
+#		1. invoke association loader for marker/mirbase data
+#		2. invoke coordinate loader
+#		3. invoke mapping loader
+#		4. update MRK_Location_Cache table
+#
+# History
+#
+# 05/03/2006	lec
+#	- TR 7651
+#
+
+cd `dirname $0`
+
+CONFIG_COMMON=`pwd`/common.config.sh
+CONFIG_LOAD=`pwd`/mirbaseload.config
+
+#
+# verify & source the DLA common configuration file
+#
+
+if [ ! -r ${CONFIG_COMMON} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_COMMON}"
+    exit 1
+fi
+
+. ${CONFIG_COMMON}
+
+#
+# verify & source the UniSTS load configuration file
+#
+
+if [ ! -r ${CONFIG_LOAD} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+. ${CONFIG_LOAD}
+
+#
+# create log file
+#
+
+LOG=${LOGDIR}/`basename $0`.log
+rm -rf ${LOG}
+touch ${LOG}
+
+#
+#  Source the DLA library functions.
+#
+
+if [ "${DLAJOBSTREAMFUNC}" != "" ]
+then
+    if [ -r ${DLAJOBSTREAMFUNC} ]
+    then
+        . ${DLAJOBSTREAMFUNC}
+    else
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${LOG}
+        exit 1
+    fi
+else
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${LOG}
+    exit 1
+fi
+
+#
+# Perform pre-load tasks.
+#
+#preload
+
+#
+# remove old output and generated input files
+#
+rm -rf ${OUTPUT_ASSOC_FILE} ${OUTPUT_COORD_FILE} ${OUTPUT_MAPPING_FILE}
+rm -rf ${INPUT_ASSOC_FILE} ${INPUT_COORD_FILE} ${INPUT_MAPPING_FILE}
+
+#
+# parse pass for associations and coordinates
+#
+echo 'Parsing input file for Associations and Coordinates...' | tee -a ${LOG}
+${MIRBASEINSTALLDIR}/mirbaseparse.py | tee -a ${LOG}
+STAT=$?
+checkStatus ${STAT} "mirbaseparse.py"
+
+# create a symbolic link between the parser output file and the loader input file
+
+ln -s ${OUTPUT_ASSOC_FILE} ${INPUT_ASSOC_FILE}
+ln -s ${OUTPUT_COORD_FILE} ${INPUT_COORD_FILE}
+ln -s ${OUTPUT_MAPPING_FILE} ${INPUT_MAPPING_FILE}
+
+# run association marker/mirbase load
+#${ASSOCLOADER_SH} ${CONFIG_LOAD} ${RADAR_DBSCHEMADIR}/Configuration.sh ${MGD_DBSCHEMADIR}/Configuration.sh ${ASSOCCONFIG}
+#STAT=$?
+#checkStatus ${STAT} "${ASSOCLOADER_SH}"
+
+# run the coordinate load
+#${COORDLOADER_SH} ${CONFIG_LOAD} ${RADAR_DBSCHEMADIR}/Configuration.sh ${MGD_DBSCHEMADIR}/Configuration.sh ${COORDLOADCONFIG}
+#STAT=$?
+#checkStatus ${STAT} "${COORDLOADER_SH}"
+
+# run the mapping load
+#${MAPPINGLOADER_SH}
+#STAT=$?
+#checkStatus ${STAT} "${MAPPINGLOADER_SH}"
+
+# run the marker location cache
+#${LOCATIONCACHE_SH}
+#STAT=$?
+#checkStatus ${STAT} "${LOCATIONCACHE_SH}"
+
+#
+# Perform post-load tasks.
+#
+#shutDown
+
+exit 0
+
---
+++ mirbaseparse.py
@@ -0,0 +1,261 @@
+#!/usr/local/bin/python
+
+#
+# Program: mirbaseparse.py
+#
+# Original Author: Lori E. Corbani
+#
+# Purpose:
+#
+# 1.  To generate a association loader input file, 
+#     a coordinate loader input file and a mapping loader input file
+#     from the MGI-hand-curated file MicroRNAload.txt
+#     which are then loaded into MGI via the association loader,
+#     the coordinate loader and the mapping loader.
+#
+# Requirements Satisfied by This Program:
+#
+# Usage:  mirbaseparse.py
+#
+# Envvars:
+#
+# Inputs:
+#
+# INPUT_FILE:  a file of miRBase coordinates and marker associations
+#
+# Outputs:
+#
+# OUTPUT_ASSOC_FILE:  an input file for marker/mirbase ID assocload
+# OUTPUT_COORD_FILE:  an input file for coordload
+# OUTPUT_MAPPING_FILE:  an input file for mappingload
+#
+# Processing:
+#
+# Exit Codes:
+#
+# Assumes:
+#
+# Bugs:
+#
+# Implementation:
+#
+#    Modules:
+#
+# Modification History:
+#
+# 05/02/2006	lec
+#	- TR 4460/MGI 3.5
+#
+
+import sys
+import os
+import re
+import db
+import string
+import mgi_utils
+import loadlib
+
+TAB = '\t'
+CRT = '\n'
+
+inFileName = os.environ['INPUT_FILE']
+assocFileName = os.environ['OUTPUT_ASSOC_FILE']
+coordFileName = os.environ['OUTPUT_COORD_FILE']
+mappingFileName = os.environ['OUTPUT_MAPPING_FILE']
+
+head, tail = os.path.split(sys.argv[0])
+diagFileName = os.environ['OUTPUTDIR'] + '/' + tail + '.diagnostics'
+errorFileName = os.environ['OUTPUTDIR'] + '/' + tail + '.error'
+
+diagFile = ''		# file descriptor
+errorFile = ''		# file descriptor
+
+assocline = '%s\t%s\n'
+coordline = '%s\t%s\t%s\t%s\t%s\t\n'
+mappingline = '%s\t%s\tn\t\tassembly\t\n'
+
+def exit(status, message = None):
+	#
+	# requires: status, the numeric exit status (integer)
+	#           message (string)
+	#
+	# effects:
+	# Print message to stderr and exits
+	#
+	# returns:
+	#
+ 
+    if message is not None:
+	sys.stderr.write('\n' + str(message) + '\n')
+ 
+    try:
+	diagFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+	errorFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+	diagFile.close()
+	errorFile.close()
+    except:
+	pass
+
+    try:
+	db.useOneConnection()
+    except:
+	pass
+
+    sys.exit(status)
+ 
+def init():
+	#
+	# requires: 
+	#
+	# effects: 
+	# 1. Processes command line options
+	# 2. Initializes global file descriptors/file names
+	#
+	# returns:
+	#
+ 
+    global diagFile, errorFile, diagFileName, errorFileName
+ 
+    try:
+	diagFile = open(diagFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % diagFileName)
+		
+    try:
+	errorFile = open(errorFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % errorFileName)
+		
+    diagFile.write('Start Date/Time: %s\n\n' % (mgi_utils.date()))
+    errorFile.write('Start Date/Time: %s\n\n' % (mgi_utils.date()))
+
+def writeAssoc(assocDict):
+	#
+	# requires:
+	#    assocDict (dictionary):  a key:value dictionary
+	#
+	# effects: 
+	# 1. Writes each association to the output association file
+	#
+	# returns:  nothing
+	#
+
+    try:
+	assocFile = open(assocFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % assocFileName)
+		
+    assocFile.write('MGI\tmiRBase\n')
+    for r in assocDict.keys():
+        assocFile.write(assocDict[r])
+
+    assocFile.close()
+
+def writeCoord(coordDict):
+	#
+	# requires:
+	#    coordDict (dictionary):  a key:value dictionary
+	#
+	# effects: 
+	# 1. Writes each coordinate to the output coordinate file
+	#
+	# returns:  nothing
+	#
+
+    try:
+	coordFile = open(coordFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % coordFileName)
+		
+    for c in coordDict.keys():
+        coordFile.write(coordDict[c])
+
+    coordFile.close()
+
+def writeMapping(mappingDict):
+	#
+	# requires:
+	#    mappingDict (dictionary):  a key:value dictionary
+	#
+	# effects: 
+	# 1. Writes each Mapping record to the output mapping file
+	#
+	# returns:  nothing
+	#
+
+    try:
+	mappingFile = open(mappingFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % mappingFileName)
+		
+    for r in mappingDict.keys():
+        mappingFile.write(mappingDict[r])
+
+    mappingFile.close()
+
+def process():
+	#
+	# requires:
+	#
+	# effects: 
+	#   input:  Micro RNA input file
+	#   output: association loader file, coordinate loader file, mapping loader file
+	#
+	# returns:  nothing
+	#
+
+    assocDict = {}
+    coordDict = {}
+    mappingDict = {}
+
+    try:
+	inFile = open(inFileName, 'r')
+    except:
+	exit(1, 'Could not open file %s\n' % inFileName)
+		
+    lineNum = 0
+
+    for line in inFile.readlines():
+
+        tokens = string.split(line[:-1], TAB)
+
+        try:
+	    mirChr = tokens[0]
+	    startCoord = tokens[3]
+	    endCoord = tokens[4]
+	    strand = tokens[5]
+	    mirID = tokens[6]
+	    markerID = 'MGI:' + tokens[8]
+	    markerSymbol = tokens[9]
+	    markerChr = tokens[10]
+
+        except:
+	    errorFile.write('Invalid Line (%d): %s\n' % (lineNum, line))
+	    lineNum = lineNum + 1
+	    continue
+
+	if string.find(mirChr, 'NT_') >= 0:
+	    errorFile.write('NT symbol skipped (%d): %s\n' % (lineNum, mirID))
+	    lineNum = lineNum + 1
+	    continue
+
+	assocDict[markerID] = assocline % (markerID, mirID)
+	coordDict[markerID] = coordline % (markerID, markerChr, startCoord, endCoord, strand)
+	mappingDict[markerID] = mappingline % (markerID, markerChr)
+
+        lineNum = lineNum + 1
+
+    writeAssoc(assocDict)
+    writeCoord(coordDict)
+    writeMapping(mappingDict)
+
+    inFile.close()
+
+#
+# Main Routine
+#
+
+init()
+process()
+exit(1)
+
